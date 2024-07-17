da18f125fafa62fcf66234e02f9bcbe66c4eacc7
palvon <palvon04@gmail.com>
2024-07-17 21:47:42 +0300

    login form for user

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,16 +4,22 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixture extends BaseFixture
 {
+    public function __construct(public UserPasswordHasherInterface $hasher)
+    {
+    }
+
     public function loadData(ObjectManager $manager): void
     {
         $this->createMany(10, "main_user", function ($i) {
             $user = new User();
             $user
                 ->setEmail(sprintf("testuser%d@mail.ru", $i))
-                ->setFirstName($this->faker->firstName);
+                ->setFirstName($this->faker->firstName)
+                ->setPassword($this->hasher->hashPassword($user, "123"));
 
             return $user;
         });
